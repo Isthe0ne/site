f3cb722f15ee44729afa51b4deeac09eff503180
IsThe0ne <flasproj@gmail.com>
2024-07-08 00:20:42 +0200

    Phish Tracking - Integration

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/<user_id>')
 def index():
-    return render_template('formulaire.html')
+    return render_template('formulaire.html', user_id=user_id)
 
 @app.route('/submit', methods=['POST'])
 def submit():
     try:
+        user_id = request.form.get('user_id')
+        response = requests.post(f'https://app-api-ebp4.onrender.com/mail-phished/{user_id}', json={"user_id": user_id})
+
+        
         # Redirigez vers la page 'phished'
         return redirect(url_for('phished'))
     except Exception as e:
